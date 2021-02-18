e0c1864ffa8bae9514ff2f1e0c7da4c6d990082e
dsrkafuu <dsrkafuu@outlook.com>
2021-02-18 18:30:48 +0800

    update: release tracker

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ Vue.config.productionTip = false;
 
 import * as Sentry from '@sentry/vue';
 import { Integrations } from '@sentry/tracing';
+import { version } from './assets/changelog';
 
 if (process.env.NODE_ENV === 'production') {
   Sentry.init({
@@ -16,6 +17,7 @@ if (process.env.NODE_ENV === 'production') {
     dsn: 'https://9c3ce0eb84764dbabc65508570b46ec4@o526740.ingest.sentry.io/5642179',
     tracesSampleRate: 1.0,
     integrations: [new Integrations.BrowserTracing()],
+    release: `dsr-tools@${/^v([0-9.]*)-?/.exec(version)[1]}`,
   });
 }
 
