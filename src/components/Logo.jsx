931acd3d1f0b5f7de3946d12fb4d4c0fa58c20ba
dsrkafuu <dsrkafuu@outlook.com>
2021-03-24 23:17:11 +0800

    feat: logo

---
+++ src/components/Logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Logo.scss';
+import jsdelivr from '@/utils/jsdelivr';
+
+function Logo() {
+  return (
+    <div className='logo'>
+      <img className='logo__image' src={jsdelivr('/dsr-tools/home/dsrca_head.webp', 'api')} />
+    </div>
+  );
+}
+
+export default Logo;
