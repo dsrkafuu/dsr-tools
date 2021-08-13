80330d5a432aa7068ec16a2c6c77485ede17a115
dsrkafuu <dsrkafuu@outlook.com>
2021-08-13 15:17:12 +0800

    chore: update changelog

--- src/assets/changelog.js
+++ src/assets/changelog.js
@@ -1,5 +1,10 @@
 export const changelog = [
   {
+    version: 'v4.3.0',
+    date: '2021-08-13',
+    logs: ['FFXIV 模块支持手动调整时区设置', '优化自动 SCSS 引入', '修复路由参数传递'],
+  },
+  {
     version: 'v4.2.0',
     date: '2021-06-22',
     logs: [
