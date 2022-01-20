03be7105c2d747ba911b32f4e2684d3463af9828
dsrkafuu <dsrkafuu@outlook.com>
2022-01-20 23:48:17 +0800

    chore: update ga script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@
  */
 
 import '../styles/index.scss';
+import Script from 'next/script';
 import type { AppProps } from 'next/app';
 import GlobalHead from '../components/GlobalHead';
 import GlobalLayout from '../components/GlobalLayout';
@@ -10,6 +11,18 @@ import GlobalLayout from '../components/GlobalLayout';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script strategy='afterInteractive'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}
+      </Script>
       <GlobalHead />
       <GlobalLayout>
         <Component {...pageProps} />
