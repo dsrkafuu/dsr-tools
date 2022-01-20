9aa2a7c4b35a33d7d6cc3378f38db168e3565f7f
dsrkafuu <dsrkafuu@outlook.com>
2022-01-20 22:06:24 +0800

    chore: add ga

---
+++ pages/_document.tsx
@@ -0,0 +1,36 @@
+/**
+ * https://nextjs.org/docs/advanced-features/custom-document
+ */
+
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+`,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
